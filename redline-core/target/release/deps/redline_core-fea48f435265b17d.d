d1449fc574e2e0a76bf6f96ad0a32db5fb5ff92b
BatistaCakewalk <118333712+BatistaCakewalk@users.noreply.github.com>
2025-12-30 23:18:56 -0500

    Setting up Gitignore
    cuz yes

--- redline-core/target/release/deps/redline_core-fea48f435265b17d.d
+++
@@ -1,5 +0,0 @@
-/home/batista/Documents/REDLINE/redline-core/target/release/deps/redline_core-fea48f435265b17d.d: src/main.rs
-
-/home/batista/Documents/REDLINE/redline-core/target/release/deps/redline_core-fea48f435265b17d: src/main.rs
-
-src/main.rs:
--- redline-core/target/release/redline-core.d
+++
@@ -1,1 +0,0 @@
-/home/batista/Documents/REDLINE/redline-core/target/release/redline-core: /home/batista/Documents/REDLINE/redline-core/src/main.rs
